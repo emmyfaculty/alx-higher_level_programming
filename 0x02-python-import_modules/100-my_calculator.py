77f578e0d82bb678288fb90304b01b86c7c63fb9
emmyfaculty <emmanuelahola91@gmail.com>
2021-11-22 16:18:50 +0000

    corrections due to check failure

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+from calculator_1 import add, sub, mul, div
 
-    num_args = len(sys.argv)
-    if num_args != 4:
+
+def main():
+    import sys
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if op is '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sign = ['+', '-', '*', '/']
+        signok = 0
+        for i in sign:
+            if sys.argv[2] == i:
+                signok = 1
+                break
+        if signok == 0:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        else:
+            a = sys.argv[1]
+            b = sys.argv[3]
+            if sys.argv[2] == sign[0]:
+                print("{:s} + {:s} = {:d}".format(a, b, add(int(a), int(b))))
+            elif sys.argv[2] == sign[1]:
+                print("{:s} - {:s} = {:d}".format(a, b, sub(int(a), int(b))))
+            elif sys.argv[2] == sign[2]:
+                print("{:s} * {:s} = {:d}".format(a, b, mul(int(a), int(b))))
+            elif sys.argv[2] == sign[3]:
+                print("{:s} / {:s} = {:d}".format(a, b, div(int(a), int(b))))
+if __name__ == "__main__":
+    main()
