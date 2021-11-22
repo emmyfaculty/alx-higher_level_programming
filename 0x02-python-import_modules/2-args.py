e1f039b07fbe48cb6537f9c11067134165d940bc
emmyfaculty <emmanuelahola91@gmail.com>
2021-11-22 15:29:52 +0000

    code was failing some check so i made some corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    num = len(sys.argv)
-    if num == 1:
-        print("{} arguments.".format(num - 1))
-    elif num == 2:
-        print("{} argument:".format(num - 1))
-    else:
-        print("{} arguments:".format(num - 1))
+import sys
 
-    for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+
+def main():
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("{}: {}".format(len(sys.argv) - 1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    main()
