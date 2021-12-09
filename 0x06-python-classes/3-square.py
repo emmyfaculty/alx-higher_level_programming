d71ac391b46804f8a97bc10dc80b76d0b4824174
Ahola Emmanuel <88318786+emmyfaculty@users.noreply.github.com>
2021-12-09 13:51:15 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    Public instance method: def area(self).
-    """
+# 103-magic_calculation.py
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
-    def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+import math
+
+
+class MagicClass:
+    """Represent a circle."""
+
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
     def area(self):
-        """Returns the current square area."""
-        return self.__size ** 2
+        """Return the area of the MagicClass."""
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """Return The circumference of the MagicClass."""
+        return (2 * math.pi * self.__radius)
